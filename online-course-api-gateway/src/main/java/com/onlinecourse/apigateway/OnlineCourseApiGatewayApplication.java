405d8b0ba9e8ccf77f98f8771127ec093e9c7b8d
KhaiCX <khaicx2001@gmail.com>
2025-07-24 21:02:32 +0700

    KhaiCX: create api-gateway

---
+++ online-course-api-gateway/src/main/java/com/onlinecourse/apigateway/OnlineCourseApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.onlinecourse.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineCourseApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineCourseApiGatewayApplication.class, args);
+	}
+
+}
---
+++ online-course-api-gateway/src/test/java/com/onlinecourse/apigateway/OnlineCourseApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onlinecourse.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineCourseApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
