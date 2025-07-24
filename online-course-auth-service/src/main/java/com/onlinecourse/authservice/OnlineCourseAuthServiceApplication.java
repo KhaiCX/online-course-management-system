41284bf84b4d665c0988fdfbec755039609e0b4b
KhaiCX <khaicx2001@gmail.com>
2025-07-24 21:15:10 +0700

    KhaiCX: create auth-service

---
+++ online-course-auth-service/src/main/java/com/onlinecourse/authservice/OnlineCourseAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.onlinecourse.authservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineCourseAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineCourseAuthServiceApplication.class, args);
+	}
+
+}
---
+++ online-course-auth-service/src/test/java/com/onlinecourse/authservice/OnlineCourseAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onlinecourse.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineCourseAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
